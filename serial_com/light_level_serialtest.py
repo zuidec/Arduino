a8c22c7d8f878386f4d94d4545dcd386a8488924
zuidec <case@CASE-DESKTOP>
2023-01-14 13:56:45 -0800

    Added code comments to light_level-serialtest.py

--- serial_com/light_level_serialtest.py
+++ serial_com/light_level_serialtest.py
@@ -1,21 +1,26 @@
+##
+## This script is dependent on pyserial which is installed separately from python
+##
 import serial
 import time
 
-arduino = serial.Serial(port = 'COM3', timeout=0)
+arduino = serial.Serial(port = 'COM3', timeout=0) #set up object to connect to arduino over COM3
 time.sleep(2)
 
-var = ""
-i=0
-buffer = b''
-endByte = b'\r'
+var = ""        # Initialize variables with the correct type before using in main loop
+buffer = b''    #
+endByte = b'\r' #
+
+# Output a label before moving into main loop where data is overwritten with a carriage return
 print("The current light level is: ")
+
 while True:
-    if arduino.in_waiting >0:
-        while buffer != endByte:
-            buffer = arduino.read()
-            var += buffer.decode('ascii')
-        if var != "":
-            print('\r',var, end="")
-            var = ""
-            buffer = b''
+    if arduino.in_waiting >0: # Check if there is data to be read from serial port
+        while buffer != endByte:                # Loop to continue reading one byte at a time until reaching \r
+            buffer = arduino.read()             #
+            var += buffer.decode('ascii')       # Convert ascii from arduino into utf-8 for python3
+        if var != "":               # Check if there is data in var to be output
+            print('\r',var, end="") # Output var
+            var = ""                # Reset var and buffer
+            buffer = b''            #
             
