83622f8e1d3017e4ae497feecd08253918abd81b
zuidec <case@CASE-DESKTOP>
2023-01-14 21:02:34 -0800

    Updated serial.cpp, now able to connect to COM port but unable to read data yet

--- serial_com/serial.cpp
+++ serial_com/serial.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <fstream>
+#include <windows.h>
+#include <string>
 
 int main()
 {
@@ -8,12 +10,12 @@ int main()
 	try 
 	{
 		std::cout << "Attempting to connect to COM3..." << std::endl; 	// Attempt to connect to arduino
-		arduino = CreateFile ("COM3",
-				GENERIC_READ | GENERIC_WRITE,
+		arduino = CreateFile ("COM3",			// COM port 3
+				GENERIC_READ | GENERIC_WRITE,	// Open read and write
 				0,
 				0,
-				OPEN_EXISTING,
-				FILE_FLAG_OVERLAPPED,
+				CREATE_ALWAYS,					// Alwayts create a new instance
+				FILE_FLAG_OVERLAPPED,			// Use overlapping
 				0);						
 
 		if(arduino == INVALID_HANDLE_VALUE) throw -1;
@@ -21,11 +23,20 @@ int main()
 	}
 	catch (...)
 	{
-		std::cout << "Error connecting on COM3, exiting now" << std:: endl;	// Catch exception and exit if
-		arduino.close();							// unsuccessful
-		return -1;									// 
+		DWORD comError = GetLastError();
+		std::cout << "Error connecting on COM3, exiting now, error number: "<< comError << std:: endl;	// Catch exception and exit if
+		CloseHandle(arduino);																								// unsuccessful
+		return -1;									 
 	}
-	
-	//arduino.close();	//Terminate arduino filestream before closing
+	std::string buffer = "";
+	while (1) /// Enter into main loop for receiving data from arduino
+	{
+		//Read a line from arduino and store into the buffer
+		std::cin.getline(buffer,25,'\r') << arduino.;
+		
+		std::cout << std::endl << "\rThe current light level is: " << buffer;
+	}
+
+	CloseHandle(arduino);
 	return 0;
 }
