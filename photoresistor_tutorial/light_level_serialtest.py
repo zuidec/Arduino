57fb32bc508f13ffe0d210664153adfdad1b4839
zuidec <case@CASE-DESKTOP>
2023-01-18 07:39:58 -0800

    Made some minor tweaks to ArduinoCOM

--- photoresistor_tutorial/light_level_serialtest.py
+++ photoresistor_tutorial/light_level_serialtest.py
@@ -7,20 +7,23 @@ import time
 arduino = serial.Serial(port = 'COM3', timeout=0) #set up object to connect to arduino over COM3
 time.sleep(2)
 
-var = ""        # Initialize variables with the correct type before using in main loop
+var = b''        # Initialize variables with the correct type before using in main loop
 buffer = b''    #
-endByte = b'\0' #
-
+endByte = b'\x03' #
+print(endByte)
 # Output a label before moving into main loop where data is overwritten with a carriage return
-print("The current light level is: ")
+#print("The current light level is: ")
 
 while True:
     if arduino.in_waiting >0: # Check if there is data to be read from serial port
+        print("Bytes remaining in buffer from Arduino: " + str(arduino.in_waiting))
         while buffer != endByte:                # Loop to continue reading one byte at a time until reaching \r
             buffer = arduino.read()             #
-            var += buffer.decode('ascii')       # Convert ascii from arduino into utf-8 for python3
-        if var != "":               # Check if there is data in var to be output
-            print('\r     \r',var, end="") # Output var
-            var = ""                # Reset var and buffer
+            print(buffer.decode('ascii'),sep="",end="")
+         #   var += buffer       # Convert ascii from arduino into utf-8 for python3
+        if var != b'':               # Check if there is data in var to be output
+ #           print('\r     \r',var, end="") # Output var
+            print(var.decode('ascii'))
+            var = b''                # Reset var and buffer
             buffer = b''            #
             
