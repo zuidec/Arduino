fcead7e8af382e6325ce6350f757a7270dfd7dcb
zuidec <case@CASE-DESKTOP>
2023-06-01 07:28:35 -0700

    Soil monitor to basic working status with radio communication

--- arduino_soil_monitor/src/main.cpp
+++ arduino_soil_monitor/src/main.cpp
@@ -3,16 +3,19 @@
  *  then transmit the reading using NRF24L01 to a base station
  * 
  *  Author: Case Zuiderveld
- *  Last updated 4/26/2023
+ *  Last updated 5/28/2023
  */
 
 #include <Arduino.h>
 // SPI and RF24 are needed to interface with NRF24L01 radio module 
 #include <SPI.h>
 #include <RF24.h>
-#include <nRF24L01.h>
+// LowPower library for sleeping functionality
+#include <LowPower.h>
 // SoilMonitor.h has the functions to read soil sensor and use autowater
 #include "SoilMonitor.h"
+// PlantPacket.h has the functions to make packets for sending wirelessly
+#include "PlantPacket.h"
 
 // Define all the pins so we can call them by name
 #define SOIL_SENSOR_PWR_PIN   (5)
@@ -22,42 +25,41 @@
 #define NRF24L01_SCK_PIN      (13)
 #define NRF24L01_CSN_PIN      (10)
 #define NRF24L01_CE_PIN       (9)
-#define NRF24L01_PWR_PIN      (6)
 #define PUMP_PWR_PIN          (3)
 #define FLOAT_SENSOR_PIN      (4)
-#define TIME_TO_SLEEP
+
+#define TIME_TO_SLEEP_SECONDS 14400   // 3,600s in one hour
 #define BUFFER_LENGTH         16
 
+// Objects
 RF24 radio(NRF24L01_CE_PIN, NRF24L01_CSN_PIN);
 SoilMonitor soilMonitor(SOIL_SENSOR_PWR_PIN,SOIL_SENSOR_DATA_PIN, PUMP_PWR_PIN, FLOAT_SENSOR_PIN);
-struct PlantPacket  {
-  char plantName[15];
-  uint8_t percentSoilLevel;
-};
 PlantPacket packet;
 
-
-bool isTXMode = true;
-bool isRadioPowered = false;
+// Variables and constants
 uint8_t radioAddress[6] = {"ollie"};
 uint8_t baseStationAddress[5] = {'b','a','s','e','\0'};
 char plantName[15] = {'o','l','i','v','e','r','\0','\0','\0','\0','\0','\0','\0','\0','\0'};
 char buffer[BUFFER_LENGTH]    = {"\0"};
 
-void ToggleRadioOn();
-void ToggleRadioOff();
+// Functions
 bool InitializeRadio();
 void ClearBuffer(char *buffer, int bufferLength);
-void SetPlantPacketName(char *plantName);
-void CreatePlantPacket(char* buffer, PlantPacket *packet);
-void EnterSleepMode(long long timeToSleepMicroseconds);
+void EnterSleepMode(uint16_t timeToSleepSeconds);
 
+//
+// 
+//
 void setup() {
     // put your setup code here, to run once:
     Serial.begin(115200);
-    pinMode(NRF24L01_PWR_PIN, OUTPUT);
+    soilMonitor.autoWater = false;
+
+    if(!InitializeRadio())  {
+      Serial.println(F("Failed to initialize radio"));
+    }
     
-    SetPlantPacketName(&plantName[0]);
+    packet.SetPlantPacketName(&plantName[0]);
 }
 
 void loop() {
@@ -65,47 +67,29 @@ void loop() {
     soilMonitor.ReadSoilLevel();
     packet.percentSoilLevel = soilMonitor.percentSoilLevel;
     ClearBuffer(&buffer[0], BUFFER_LENGTH);
-    CreatePlantPacket(&buffer[0], &packet);
+    packet.CreatePlantPacket(&buffer[0]);
+    
+    // Ouput buffer contents for packet debugging
     Serial.print("Buffer contents: ");
     for(int i=0;i<16;i++)  {
       Serial.print(buffer[i]);
     }
     Serial.println();
-    // Turn on radio and attempt to transmit the soil level
-    if(!InitializeRadio())  {
-      Serial.println(F("Failed to initialize radio"));
-    }
-    else if(!radio.write(&buffer[0], sizeof(buffer)))  {
+    
+    // Attempt to transmit the soil level
+    if(!radio.write(&buffer[0], sizeof(buffer)))  {
       Serial.println(F("Transmission failed"));
     }
     else  {
       Serial.println(F("Transmission successful"));
     }
-    ToggleRadioOff();
     
-    Serial.println(soilMonitor.percentSoilLevel);
-    delay(2000);
-}
-
-void ToggleRadioOn() {
-    if(!isRadioPowered)  {
-      digitalWrite(NRF24L01_PWR_PIN, HIGH);
-      isRadioPowered = true;
-    }
-}
-
-void ToggleRadioOff() {
-    if(isRadioPowered)  {
-      digitalWrite(NRF24L01_PWR_PIN, LOW);
-      isRadioPowered = false;
-    }
+    // Go to sleep
+    EnterSleepMode(TIME_TO_SLEEP_SECONDS);
 }
 
 bool InitializeRadio()  {
-    if(!isRadioPowered) {
-       ToggleRadioOn();
-       delay(50);   // Wait 50ms for radio board to become powered
-    }
+
     // Attempt to connect to radio up to three times
     for(uint8_t i=0; i<3 && !radio.begin(); i++)  {
       if(i==3)  {
@@ -119,22 +103,9 @@ bool InitializeRadio()  {
     radio.setDataRate(RF24_2MBPS);
     radio.setPayloadSize(sizeof(buffer));
     radio.openWritingPipe(baseStationAddress);
-    //radio.openReadingPipe(1, radioAddress);
+    radio.stopListening();
 
     return true;
-}
-
-void SetPlantPacketName(char *plantName)  {
-  for(uint8_t i=0;i<15;i++) {
-    packet.plantName[i] = plantName[i];
-  }
-}
-
-void CreatePlantPacket(char* buffer, PlantPacket *packet) {
-  for(uint8_t i=0; i<15;i++)  {
-    buffer[i] = packet->plantName[i];
-  }
-  buffer[15] = (char)packet->percentSoilLevel;
 }
 
 void ClearBuffer(char *buffer, int bufferLength) {
@@ -144,4 +115,21 @@ void ClearBuffer(char *buffer, int bufferLength) {
   }
 
   return;
+}
+
+void EnterSleepMode(uint16_t timeToSleepSeconds) {
+
+  // Put radio into powerdown mode
+  radio.powerDown();
+  
+  // Loop through sleeping 8s at a time until its time to wake up
+  for (uint16_t i = 0; (timeToSleepSeconds-i) >= 8; i+=8)
+  {
+    // Put arduino into power down mode
+    LowPower.powerDown(SLEEP_8S, ADC_OFF, BOD_OFF);
+  }
+  
+  // Turn radio back on
+  radio.powerUp();
+  
 }
